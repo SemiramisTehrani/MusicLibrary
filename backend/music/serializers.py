3f120446d2445139b663cd9488bdd87c8e34969b
unknown <semtehrani@yahoo.com>
2022-07-28 12:21:45 -0700

    created postman , it works so far to do GET

--- backend/music/serializers.py
+++ backend/music/serializers.py
@@ -3,7 +3,7 @@
 from rest_framework import serializers   
 from .models import Music
 
-class CarSerializer(serializers.ModelSerializer):
+class MusicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Music
         fields = ['id','title','artist', 'album', 'release_date', 'genre','like_status']
--- backend/music/views.py
+++ backend/music/views.py
@@ -2,8 +2,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .serializers import MusicSerializer
+from .models import Music
+
+
 
 @api_view(['GET'])
 def music_list(request) :
 
-    return Response('ok')
+    music = Music.objects.all()
+    serializer = MusicSerializer(music, many = True)
+
+    #return Response('ok')
+    return Response(serializer.data)
