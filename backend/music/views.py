914327f0c8bd09481c459971341b86dd6d3bbcd6
unknown <semtehrani@yahoo.com>
2022-07-28 13:14:00 -0700

    GTE & POST work with correct server status number

--- backend/music/views.py
+++ backend/music/views.py
@@ -5,13 +5,23 @@ from rest_framework.response import Response
 from .serializers import MusicSerializer
 from .models import Music
 
+from rest_framework import status
 
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def music_list(request) :
+    if request.method == 'GET' :
+        music = Music.objects.all()
+        serializer = MusicSerializer(music, many = True)
+        #return Response('ok')
+        return Response(serializer.data)
 
-    music = Music.objects.all()
-    serializer = MusicSerializer(music, many = True)
+    elif request.method == 'POST' :
+        serializer = MusicSerializer(data = request.data)
+        serializer.is_valid(raise_exception =  True)
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
 
-    #return Response('ok')
-    return Response(serializer.data)
+
+
+    
