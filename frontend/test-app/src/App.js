d81496be45dbe9c3be89e37552edd10ae424ce91
unknown <semtehrani@yahoo.com>
2022-08-04 09:15:47 -0700

    GET works in react.

--- frontend/test-app/src/App.js
+++ frontend/test-app/src/App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios'
 
 function App() {
+  const [songs,setSongs] =useState([])
+
+  useEffect(() => {
+    getAllSongs();
+  }, []);
+    
+    async function getAllSongs(){ 
+      const response = await axios.get("http://127.0.0.1:8000/api/music/");
+      console.log(response.data);
+      setSongs(response.data)
+  }
+
   return (
-    <div className="App">
-      Hello World!
+    <div >
+      <button onClick ={() => getAllSongs()} > Get All Song </button>
     </div>
   );
 }
