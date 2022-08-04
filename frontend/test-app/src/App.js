4846c4a14d6f62e99798c55c7fff2c2403ba79bf
unknown <semtehrani@yahoo.com>
2022-08-04 11:47:48 -0700

    finally table & filter work , fixed typos all over the map , add is not working yet

--- frontend/test-app/src/App.js
+++ frontend/test-app/src/App.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios'
 
+import MusicTable from './components/MusicTable/MusicTable';
+import AddSong from './components/AddSong/AddSong';
+import NavBar from './components/SearchBar/SearchBar.jsx';
+
 function App() {
   const [songs,setSongs] =useState([])
 
@@ -9,14 +13,23 @@ function App() {
   }, []);
     
     async function getAllSongs(){ 
-      const response = await axios.get("http://127.0.0.1:8000/api/music/");
-      console.log(response.data);
+      let response = await axios.get("http://127.0.0.1:8000/api/music/");
+      // console.log(response.data);
       setSongs(response.data)
   }
+    async function addSong(newSong){
+      let response = await axios.post('http://127.0.0.1:8000/api/music/', newSong);
+      if(response.status === 201){
+      await getAllSongs();
+    }
+  } 
 
   return (
-    <div >
-      <button onClick ={() => getAllSongs()} > Get All Song </button>
+    <div className='page-container'>
+      <NavBar className = 'navbar'/>
+      <div><MusicTable displaySongs = {songs}/></div> 
+      <div className='content-wrap'><AddSong addNewSong={addSong}/></div>
+      
     </div>
   );
 }
---
+++ frontend/test-app/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,14 @@
+
+
+import React from "react";
+import './SearchBar.css';
+
+const NavBar = () => {
+  return (
+    <div>
+        Semi's frontend & backend Music Library!
+    </div>
+  );
+};
+
+export default NavBar;
