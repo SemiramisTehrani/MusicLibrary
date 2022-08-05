52d7b23755cb453f30df5ede10ff72da5151b306
unknown <semtehrani@yahoo.com>
2022-08-05 07:56:51 -0700

    Added delete & update song components folders/files & added them in App.jsx , next is developing its function files

--- frontend/test-app/src/App.js
+++ frontend/test-app/src/App.js
@@ -4,6 +4,11 @@ import axios from 'axios'
 import MusicTable from './components/MusicTable/MusicTable';
 import AddSong from './components/AddSong/AddSong';
 import NavBar from './components/SearchBar/SearchBar.jsx';
+// bonus part : update , delete song
+import UpdateSong from './components/UpdateSong/UpdateSong';
+import DeleteSong from './components/DeleteSong/DeleteSong'
+
+
 
 function App() {
   const [songs,setSongs] =useState([])
@@ -22,6 +27,19 @@ function App() {
       if(response.status === 201){
       await getAllSongs();
     }
+  }
+    // bonus part : delete song (Delete in postman)
+    async function deletesong(){ 
+      let response = await axios.delete("http://127.0.0.1:8000/api/music/");
+    // console.log(response.data);
+      setSongs(response.data)
+  }
+
+    // bonus part : update song (put im postman)
+    async function updatesong(){ 
+      let response = await axios.put("http://127.0.0.1:8000/api/music/");
+      // console.log(response.data);
+      setSongs(response.data)
   }
 
   return (
--- frontend/test-app/src/components/AddSong/AddSong.jsx
+++ frontend/test-app/src/components/AddSong/AddSong.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import "./AddSong.css";
-import { nanoid } from "nanoid";
+// import { nanoid } from "nanoid";
 
 
 
